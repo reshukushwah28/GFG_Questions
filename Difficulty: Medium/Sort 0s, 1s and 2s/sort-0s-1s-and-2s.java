cfba2a435bd0a8d2e0bf19ffca13f3206b2f6098
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-04-21 22:57:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,27 +1,27 @@
 class Solution {
     public void sort012(int[] arr) {
-        int low = 0, mid = 0, high = arr.length - 1;
-        
-        while (mid <= high) {
-            if (arr[mid] == 0) {
-                // swap arr[low] and arr[mid]
-                int temp = arr[low];
-                arr[low] = arr[mid];
+        //dutch flag algo 
+        int l = 0;
+        int r = arr.length-1;
+        int mid = 0;
+        while(mid<= r){
+            if(arr[mid] == 0){
+                int temp = arr[l];
+                arr[l] = arr[mid];
                 arr[mid] = temp;
-                
-                low++;
+                l++;
                 mid++;
-            } 
-            else if (arr[mid] == 1) {
+            }
+            else if (arr[mid] == 1){
                 mid++;
-            } 
-            else { 
+            }
+            else {
                 int temp = arr[mid];
-                arr[mid] = arr[high];
-                arr[high] = temp;
-                
-                high--;
+                arr[mid] = arr[r];
+                arr[r] =  temp;
+                r--;
             }
         }
+        
     }
 }
